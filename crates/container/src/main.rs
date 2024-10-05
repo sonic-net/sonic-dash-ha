3ef720f8a237db1676ca1e55d258f11d0587c986
Saikrishna Arcot <sarcot@microsoft.com>
2024-10-04 17:29:17 -0700

    Implement most of the parts that involve Kubernetes
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- crates/container/src/main.rs
+++ crates/container/src/main.rs
@@ -6,6 +6,10 @@ use bollard::Docker;
 use bollard::container::*;
 use futures_util::stream::TryStreamExt;
 use enumset;
+use std::fs::File;
+use std::io::Read;
+use std::time::Duration;
+use std::thread::sleep;
 
 struct DbConnections {
     config_db : DbConnector,
@@ -39,8 +43,19 @@ const ST_SER_UPDATE_TS: &str = "update_time";
 //
 const SONIC_CTR_CONFIG: &str = "/etc/sonic/remote_ctr.config.json";
 const SONIC_CTR_CONFIG_PEND_SECS: &str = "revert_to_local_on_wait_seconds";
-const DEFAULT_PEND_SECS: i32 = 5 * 60;
-const WAIT_POLL_SECS: i32 = 2;
+const DEFAULT_PEND_SECS: u32 = 5 * 60;
+const WAIT_POLL_SECS: u32 = 2;
+
+fn get_config_data(field: &str) -> Option<serde_json::Value> {
+    let mut file = match File::open(SONIC_CTR_CONFIG) {
+        Ok(f) => f,
+        Err(_e) => return None,
+    };
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents).unwrap();
+    let data : serde_json::Value = serde_json::from_str(&file_contents).unwrap();
+    data.as_object().unwrap().get(field).and_then(|value| Some(value.clone()))
+}
 
 fn read_data(db_connector: &DbConnector, feature: &String, fields: &mut HashMap<&str, String>) {
     let table_name : &str;
@@ -81,6 +96,12 @@ fn read_state(db_connections: &DbConnections, feature: &String) -> HashMap<&'sta
     fields
 }
 
+fn set_label(db_connections: &DbConnections, feature: &String, create: bool) {
+    if db_connections.remote_ctr_enabled {
+        todo!();
+    }
+}
+
 fn update_data(db_connections: &DbConnections, feature: &String, data: &HashMap<&str, String>) {
     if db_connections.remote_ctr_enabled {
         todo!();
@@ -115,6 +136,28 @@ fn container_version(docker: &Docker, feature: &String) -> String {
     };
 }
 
+fn initialize_connection() -> DbConnections {
+    DbConnections {
+        config_db: DbConnector::new_tcp(4, "localhost", 6379, 0),
+        state_db: DbConnector::new_tcp(6, "localhost", 6379, 0),
+        remote_ctr_enabled: false,
+    }
+}
+
+fn get_container_id(feature: &String, db_connections: &DbConnections) -> String {
+    let data = db_connections.state_db.hgetall(&format!("FEATURE|{}", feature));
+    if data.get(CURRENT_OWNER).unwrap_or(&String::new()) == "local" {
+        return feature.clone();
+    } else {
+        return data.get(CONTAINER_ID).unwrap_or(feature).clone();
+    }
+}
+
+fn container_id(feature: &String) {
+    let db_connections = initialize_connection();
+    println!("{}\n", get_container_id(feature, &db_connections));
+}
+
 #[derive(enumset::EnumSetType, Debug)]
 enum StartFlags {
     StartLocal,
@@ -122,11 +165,7 @@ enum StartFlags {
 }
 
 fn container_start(feature: &String) {
-    let db_connections = DbConnections {
-        config_db: DbConnector::new_tcp(4, "localhost", 6379, 0),
-        state_db: DbConnector::new_tcp(4, "localhost", 6379, 0),
-        remote_ctr_enabled: false,
-    };
+    let db_connections = initialize_connection();
 
     let feature_config = read_config(&db_connections, &feature);
     let feature_state = read_state(&db_connections, &feature);
@@ -143,7 +182,7 @@ fn container_start(feature: &String) {
         start_val |= StartFlags::StartLocal;
     } else {
         start_val |= StartFlags::StartKube;
-        if feature_config.get(NO_FALLBACK).unwrap() == "True" && feature_state.get(REMOTE_STATE).unwrap() == "none" {
+        if feature_config.get(NO_FALLBACK).unwrap() == "False" && feature_state.get(REMOTE_STATE).unwrap() == "none" {
             start_val |= StartFlags::StartLocal;
             data.insert(REMOTE_STATE, "none".to_string());
         }
@@ -153,30 +192,33 @@ fn container_start(feature: &String) {
         data.insert(CURRENT_OWNER, "local".to_string());
         data.insert(CONTAINER_ID, feature.clone());
         if start_val == StartFlags::StartLocal {
+            set_label(&db_connections, &feature, false);
             data.insert(REMOTE_STATE, "none".to_string());
         }
     }
 
     update_data(&db_connections, &feature, &data);
 
-    let docker = Docker::connect_with_local_defaults().unwrap();
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap();
-    rt.block_on(docker.start_container(&feature, None::<StartContainerOptions<String>>)).expect("Unable to communicate with Docker");
+    if start_val & StartFlags::StartLocal != enumset::EnumSet::empty() {
+        let docker = Docker::connect_with_local_defaults().unwrap();
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        rt.block_on(docker.start_container(&feature, None::<StartContainerOptions<String>>)).expect("Unable to communicate with Docker");
+    }
+
+    if start_val & StartFlags::StartKube != enumset::EnumSet::empty() {
+        set_label(&db_connections, &feature, true);
+    }
 }
 
 fn container_stop(feature: &String) {
-    let db_connections = DbConnections {
-        config_db: DbConnector::new_tcp(4, "localhost", 6379, 0),
-        state_db: DbConnector::new_tcp(4, "localhost", 6379, 0),
-        remote_ctr_enabled: false,
-    };
+    let db_connections = initialize_connection();
 
     let feature_config = read_config(&db_connections, &feature);
     let feature_state = read_state(&db_connections, &feature);
-    let docker_id = feature;
+    let docker_id = get_container_id(feature, &db_connections);
 
     let docker = Docker::connect_with_local_defaults().unwrap();
 
@@ -204,15 +246,24 @@ fn container_stop(feature: &String) {
 }
 
 fn container_kill(feature: &String) {
-    let db_connections = DbConnections {
-        config_db: DbConnector::new_tcp(4, "localhost", 6379, 0),
-        state_db: DbConnector::new_tcp(4, "localhost", 6379, 0),
-        remote_ctr_enabled: false,
-    };
+    let db_connections = initialize_connection();
 
     let feature_config = read_config(&db_connections, &feature);
     let feature_state = read_state(&db_connections, &feature);
-    let docker_id = feature;
+    let docker_id = get_container_id(feature, &db_connections);
+    let remove_label = (feature_config.get(SET_OWNER).unwrap() != "local") || (feature_state.get(CURRENT_OWNER).unwrap() != "local");
+
+    if remove_label {
+        set_label(&db_connections, &feature, false);
+    }
+
+    if feature_config.get(SET_OWNER).unwrap() == "local" {
+        let current_state = feature_state.get(STATE).unwrap();
+        if current_state != "enabled" && current_state != "always_enabled" {
+            println!("{} is not enabled", feature);
+            return;
+        }
+    }
 
     let docker = Docker::connect_with_local_defaults().unwrap();
 
@@ -228,22 +279,41 @@ fn container_kill(feature: &String) {
 }
 
 fn container_wait(feature: &String) {
-    let db_connections = DbConnections {
-        config_db: DbConnector::new_tcp(4, "localhost", 6379, 0),
-        state_db: DbConnector::new_tcp(4, "localhost", 6379, 0),
-        remote_ctr_enabled: false,
-    };
+    let db_connections = initialize_connection();
 
     let feature_config = read_config(&db_connections, &feature);
-    let feature_state = read_state(&db_connections, &feature);
-    let docker_id = feature;
+    let mut feature_state = read_state(&db_connections, &feature);
+    let mut docker_id = get_container_id(feature, &db_connections);
+    let mut pend_wait_seconds : u32 = 0;
 
     let docker = Docker::connect_with_local_defaults().unwrap();
 
-    if docker_id == feature {
+    if docker_id == *feature {
         let version = container_version(&docker, &feature);
         if !version.is_empty() {
             update_data(&db_connections, &feature, &HashMap::from([(ST_FEAT_CTR_STABLE_VER, version)]));
+        }
+    }
+
+    if docker_id.is_empty() && feature_config.get(NO_FALLBACK).unwrap() == "False" {
+        pend_wait_seconds = get_config_data(SONIC_CTR_CONFIG_PEND_SECS).and_then(|value| value.as_u64()).and_then(|value| Some(value as u32)).unwrap_or(DEFAULT_PEND_SECS);
+    }
+
+    while docker_id.is_empty() {
+        if feature_config.get(NO_FALLBACK).unwrap() == "False" {
+            if pend_wait_seconds < WAIT_POLL_SECS {
+                break;
+            }
+            pend_wait_seconds = pend_wait_seconds - WAIT_POLL_SECS;
+        }
+
+        sleep(Duration::from_secs(WAIT_POLL_SECS as u64));
+        feature_state = read_state(&db_connections, &feature);
+
+        docker_id = feature_state.get(CONTAINER_ID).unwrap().clone();
+
+        if feature_state.get(REMOTE_STATE).unwrap() == "pending" {
+            update_data(&db_connections, &feature, &HashMap::from([(REMOTE_STATE, "ready".to_string())]));
         }
     }
 
@@ -269,9 +339,15 @@ enum Action {
 #[command(version, about, long_about = None)]
 struct Cli {
     #[arg(value_enum)]
+    /// The action to take for the container
     action: Action,
+
     /// The name of the container
     name: String,
+
+    /// Timeout for the action to occur
+    #[arg(short, long)]
+    timeout: Option<u32>,
 }
 
 fn main() {
@@ -282,6 +358,6 @@ fn main() {
         Action::Wait => container_wait(&cli.name),
         Action::Stop => container_stop(&cli.name),
         Action::Kill => container_kill(&cli.name),
-        Action::Id => todo!(),
+        Action::Id => container_id(&cli.name),
     };
 }
