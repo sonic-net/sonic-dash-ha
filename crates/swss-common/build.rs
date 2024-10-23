4ae74e79f8a4c436f3d7af3603532305c83c293d
erer1243 <erer1243@users.noreply.github.com>
2024-10-23 18:24:53 -0400

    Fix up build.rs

--- crates/swss-common/build.rs
+++ crates/swss-common/build.rs
@@ -1,26 +1,70 @@
-use std::{env, fs, path::PathBuf};
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
 
 fn main() {
-    let swss_common_path = fs::canonicalize(PathBuf::from(
-        env::var("SWSS_COMMON_PATH").unwrap_or("../../../sonic-swss-common".into()),
-    ))
-    .unwrap();
-    assert!(
-        swss_common_path.exists(),
-        "{swss_common_path:?} doesn't exist - set env var SWSS_COMMON_PATH to the root of the sonic-swss-common repo"
-    );
-    let libs_path = swss_common_path.join("common/.libs").to_str().unwrap().to_string();
-    let include_path = swss_common_path.join("common/c-api");
-    let header_paths = fs::read_dir(include_path)
-        .unwrap()
-        .map(|res_entry| res_entry.unwrap().path())
-        .filter(|p| p.extension().map_or(false, |ext| ext == "h"))
-        .map(|p| p.to_str().unwrap().to_string())
-        .collect::<Vec<_>>();
+    let mut bindgen = bindgen::builder();
+
+    match env::var_os("SWSS_COMMON_REPO") {
+        // if SWSS_COMMON_REPO is specified, we will build this library based on the files found in
+        // the repo
+        Some(path_string) => {
+            // Get full path of repo
+            let path = Path::new(&path_string);
+            let path = fs::canonicalize(path).unwrap_or_else(|e| {
+                let debug_path = if path.is_relative() {
+                    env::current_dir().unwrap()
+                } else {
+                    PathBuf::new()
+                }
+                .join(path);
+                panic!("{debug_path:?}: {e}");
+            });
+            assert!(path.exists(), "{path:?} doesn't exist");
+
+            // Add libs path to linker search
+            let libs_path = path.join("common/.libs").to_str().unwrap().to_string();
+            println!("cargo:rustc-link-search=native={libs_path}");
+
+            // include all of common/c-api/*.h
+            let include_path = path.join("common/c-api");
+            let header_paths = fs::read_dir(include_path)
+                .unwrap()
+                .map(|res_entry| res_entry.unwrap().path())
+                .filter(|p| p.extension().map_or(false, |ext| ext == "h"))
+                .map(|p| p.to_str().unwrap().to_string())
+                .collect::<Vec<_>>();
+
+            bindgen = bindgen.headers(&header_paths);
+
+            for h in header_paths {
+                println!("cargo:rerun-if-changed={h}");
+            }
+        }
+
+        // otherwise, we will assume the libswsscommon-dev package is installed and files
+        // will be found at the locations defined in <sonic-swss-common repo>/debian/libswsscommon-dev.install
+        None => {
+            const INCLUDE_DIR: &str = "/usr/include/swss/c-api/";
+            const HEADERS: &[&str] = &[
+                "consumerstatetable.h",
+                "dbconnector.h",
+                "producerstatetable.h",
+                "subscriberstatetable.h",
+                "util.h",
+                "zmqclient.h",
+                "zmqconsumerstatetable.h",
+                "zmqproducerstatetable.h",
+                "zmqserver.h",
+            ];
+            let header_paths = HEADERS.iter().map(|h| format!("{INCLUDE_DIR}/{h}")).collect::<Vec<_>>();
+            bindgen = bindgen.headers(header_paths);
+        }
+    };
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
-    bindgen::builder()
-        .headers(&header_paths)
+    bindgen
         .derive_partialeq(true)
         .generate()
         .unwrap()
@@ -28,8 +72,4 @@ fn main() {
         .unwrap();
 
     println!("cargo:rustc-link-lib=dylib=swsscommon");
-    println!("cargo:rustc-link-search=native={libs_path}");
-    for h in header_paths {
-        println!("cargo:rerun-if-changed={h}");
-    }
 }
