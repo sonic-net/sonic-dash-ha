97bd80c93bc108ce434576b8f0ba03f72349678d
Yue Gao <yuega2@cisco.com>
2025-04-30 14:59:24 -0400

    Initialize hamgrd from config_db and connect to swbusd

--- crates/swbus-edge/src/edge_runtime.rs
+++ crates/swbus-edge/src/edge_runtime.rs
@@ -14,12 +14,15 @@ pub struct SwbusEdgeRuntime {
     swbus_uri: String,
     message_router: SwbusMessageRouter,
     sender_to_message_router: Sender<SwbusMessage>,
+    //base service path with service type and service id
+    base_sp: ServicePath,
 }
 
 impl SwbusEdgeRuntime {
     pub fn new(swbus_uri: String, sp: ServicePath) -> Self {
         let (local_msg_tx, local_msg_rx) = channel(SWBUS_RECV_QUEUE_SIZE);
         let (remote_msg_tx, remote_msg_rx) = channel(SWBUS_RECV_QUEUE_SIZE);
+        let base_sp = sp.clone();
         let swbus_client = SwbusCoreClient::new(swbus_uri.clone(), sp, remote_msg_tx);
         let message_router = SwbusMessageRouter::new(swbus_client, local_msg_rx, remote_msg_rx);
 
@@ -27,12 +30,20 @@ impl SwbusEdgeRuntime {
             swbus_uri,
             message_router,
             sender_to_message_router: local_msg_tx,
+            base_sp,
         }
     }
 
     pub async fn start(&mut self) -> Result<()> {
         info!("Starting edge runtime with URI: {}", self.swbus_uri);
         self.message_router.start().await
+    }
+
+    pub fn new_sp(&self, resource_type: &str, resource_id: &str) -> ServicePath {
+        let mut new_sp = self.base_sp.clone();
+        new_sp.resource_type = resource_type.to_string();
+        new_sp.resource_id = resource_id.to_string();
+        new_sp
     }
 
     /// Add handler that can be reached from any swbus client.
