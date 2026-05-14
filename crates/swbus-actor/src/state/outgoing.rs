27c24c5ee5af0cc376beca40c170d55a9bbdc374
BYGX-wcr <wcr@live.cn>
2026-05-14 20:26:33 +0000

    allow maintenance loop to return to enable actor termination when the actor is idle
    Signed-off-by: BYGX-wcr <wcr@live.cn>

--- crates/swbus-actor/src/state/outgoing.rs
+++ crates/swbus-actor/src/state/outgoing.rs
@@ -150,6 +150,12 @@ impl Outgoing {
             if !self.queued_messages.is_empty() {
                 self.send_queued_messages().await;
             }
+            else if self.unacked_messages.is_empty() {
+                // if both queued messages and unacked messages queues are empty
+                // there is nothing to do maintenance, we will return to check if
+                // the actor is marked as deleted
+                return;
+            }
 
             // Resend unacked messages periodically
             let now = SystemTime::now();
